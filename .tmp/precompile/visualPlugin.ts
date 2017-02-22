368fba6834151fb04dc13530616cc79712be80b1
Konstantin Mamaev <konstantin.mamaev@akvelon.com>
2017-02-22 17:37:50 +0300

    Initial commit

---
+++ .tmp/precompile/visualPlugin.ts
@@ -0,0 +1,11 @@
+module powerbi.visuals.plugins {
+    export var PBI_CV_90CF7C04_2D19_4DD1_8D69_A5FE218BE58B_DEBUG = {
+        name: 'PBI_CV_90CF7C04_2D19_4DD1_8D69_A5FE218BE58B_DEBUG',
+        displayName: 'TestVisual',
+        class: 'Visual',
+        version: '1.0.0',
+        apiVersion: '1.5.0',
+        create: (options: extensibility.visual.VisualConstructorOptions) => new powerbi.extensibility.visual.PBI_CV_90CF7C04_2D19_4DD1_8D69_A5FE218BE58B.Visual(options),
+        custom: true
+    };
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+## Test visual for PowerBI
